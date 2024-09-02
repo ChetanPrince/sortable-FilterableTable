48cd3a991cb09c02bf0fa0154306eb07b6bb3c86
ChetanPrince <saini.rajvir2@gmail.com>
2024-09-02 02:07:48 +0200

    2

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 <h1>sortable-FilterableTable</h1>
 
 # Author: Chetan prince
+
+<h3>Student Record Filtering</h3>
+
+<p>Find Any student record based on their Name, Roll No, Rank Grade and other data</p>
--- script.js
+++ script.js
@@ -7,3 +7,19 @@
     //then the data should have headings exact matching to the retreived data which means either i should have th already mentioned after reading apu docs or those should dynamically be added if api suggests so doing 
 
 //  then on each heading for each method add an event listener and upon click the function should reflect so each button will have its functionality so if else statements will guide the user 
+
+
+
+
+const button = document.getElementsByTagName("button");
+button.addEventListener("click", ()=>{
+
+    const table = document.querySelector(".output, tbody[0]");
+    const tr = document.createElement("tr");
+    tr.innerHTML = `<td>Item1</td><td>item2</td><td>item3</td><td>item4</td>`
+    table.appendChild(tr);
+    // tr.innerHTML= `<td>${input[0]}</td>
+    // <td>${input[1]}</td>
+    // <td>${input[2]}</td>
+    // <td>${input[3]}</td>`
+})
