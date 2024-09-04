ae279fd0d00b50d49b1d8864a7ca14fb9c1a6bf8
ChetanPrince <saini.rajvir2@gmail.com>
2024-09-04 02:07:24 +0200

    2

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 const button = document.getElementsByTagName("button")[0];
 button.addEventListener("click", ()=>{
     const formData = getData();
-
     const table = document.querySelector(".output tbody");
     const tr = document.createElement("tr");
     tr.innerHTML = `<td>${formData.name}</td><td>${formData.studentRollNo}</td><td>${formData.studentRanking}</td><td>${formData.studentGrade}</td>`
     table.appendChild(tr);
+    clearData();
+    saveData();
 });
 
 
@@ -16,4 +17,13 @@ function getData(){
         studentRanking: document.getElementById("studentRanking").value,
         studentGrade: document.getElementById("studentGrade").value
     };
+}
+function clearData(){
+    document.getElementById("studentName").value = "";
+    document.getElementById("studentRollNo").value="";
+    document.getElementById("studentRanking").value="";
+    document.getElementById("studentGrade").value = "";
+}
+function saveData(){
+    let tr = document.querySelector(".output").getElementsByTagName("tbody")[0];
 }
