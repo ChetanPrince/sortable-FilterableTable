9fbcd9c20cecc010ce382106d0c3ec9affe9c4df
ChetanPrince <saini.rajvir2@gmail.com>
2024-09-19 20:33:05 +0200

    3

--- script.js
+++ script.js
@@ -3,37 +3,36 @@ btn.addEventListener("click", (e)=>{
     e.preventDefault();
     let formData = getData();
     // get inputs from container class
-    const inputs = document.querySelectorAll(".container input");
-    let allFieldsFilled = true;
-  inputs.forEach((input)=>{
-    if(input.value.trim()===""){
+  const inputs = document.querySelectorAll(".container input");
+  let allFieldsFilled = true;    //  go through each input to trim space before and after the content and says if conditino is empty that every input field is empty
+  inputs.forEach(input=>{
+    if(input.ariaValueMax.trim()===""){
         allFieldsFilled = false;
     }
-});
-    //  go through each input to trim space before and after the content and says if conditino is empty that every input field is empty
+})
 
-    //  out of the loop conditio is checked if all fields are filled and then alert is displayed with return keyword
- if(!allFieldsFilled){
-    alert("Kindly fill all the fields with appropriate data!");
- }
+//  out of the loop conditio is checked if all fields are filled and then alert is displayed with return keyword
+if(!allFieldsFilled){
+    alert("kindly fill the required fields");
+    return;
+}
+
  // further condition is checked if selected row is null and emptu then savedData constant is parsed into local storage and get any data if is available or it is empty array
+ 
  if(selectedRow === null){
     const savedData = JSON.parse(localStorage.getItem("studentData"))||[];
+    //  const rollNoExists check inside savedData amd .some method check if student roll no is equal to formDat.studentRollNo
     const rollNoExists = savedData.some(data=>data.studentRollNo === formData.studentRollNo);
+    // then if condition check if not roll no exists exists then saveData function is applied or else alert is diplayed
     if(!rollNoExists){
         savedData(formData);
-    }else{alert("student Roll No already exists.")}
- }
- else{
-    updataData(formData);
+    }else{
+        alert("Student Roll No already exists");
+
+    }
+ }else{
+     //  else updateData function with formData is displayed and clearData function is executed
+    updateData(formData);
  }
  clearData();
- 
-    //  const rollNoExists check inside savedData amd .some method check if student roll no is equal to formDat.studentRollNo
-
-        // then if condition check if not roll no exists exists then saveData function is applied or else alert is diplayed
-     
-    //  else updateData function with formData is displayed and clearData function is executed
-    
-
 });
